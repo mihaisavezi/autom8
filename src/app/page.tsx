bec8feac98ee1574172d2840e29d81610da27b6b
Mihai Popescu <mihai.ppscu@gmail.com>
2025-06-09 19:15:15 +0300

    feat: init repo for prep

---
+++ src/app/page.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { ContainerScroll } from "~/components/global/container-scroll-animation";
+import { InfiniteMovingCards } from "~/components/global/infinite-moving-cards";
+import Navbar from "~/components/global/navbar";
+import { Button } from "~/components/ui/button";
+import { clients } from "~/lib/constant";
+
+export default function HomePage() {
+	return (
+    <main>
+      <Navbar />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                <Button
+                  size={"lg"}
+                  className="p-8 mb-8 md:mb-0 text-xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="my-4 text-4xl md:text-6xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-bold pb-8">
+                  Automate Your Work With Fuzzie
+                </h1>
+              </div>
+            }
+          />
+        </div>
+      </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
+    </main>
+  );
+}
